d1972439e5613dccaafdcc234ea672424ecd849c
loic-merlhe <151088440+lazzy3457@users.noreply.github.com>
2026-04-09 10:41:57 +0200

    v1.1

--- sketch.js
+++ sketch.js
@@ -1,3 +1,21 @@
+function drawArrow(base, vec, myColor) {
+  if (vec.mag() < 2) return; // Ne pas dessiner si le vecteur est presque nul
+  
+  push();
+  stroke(myColor);
+  strokeWeight(2);
+  fill(myColor);
+  
+  translate(base.x, base.y);
+  line(0, 0, vec.x, vec.y); // Dessine la ligne du centre vers le vecteur
+  
+  rotate(vec.heading());
+  let arrowSize = 7;
+  translate(vec.mag(), 0); // Va au bout de la flèche
+  triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+  pop();
+}
+
 class Bille {
 
     constructor (pos, size) {
@@ -54,6 +72,16 @@ class Bille {
     }
 
     update () {
+
+        if (userSellect) {
+            if (userSellect == this) {
+                // On crée les deux points
+                let pointA = createVector(userFirstPositionClick[0], userFirstPositionClick[1]);
+                let pointB = createVector(mouseX, mouseY);
+                let tension = p5.Vector.sub(pointA, pointB);
+                drawArrow(this.pos, tension, "#000")
+            }
+        }
 
         // this.vit.normalize();    
 
